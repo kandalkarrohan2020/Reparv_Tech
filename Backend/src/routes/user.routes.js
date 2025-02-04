faba76ff3248bfc7dad7c68270597f598ae33212
kandalkarrohan <cropkhata@gmail.com>
2025-02-04 21:31:29 +0530

    Delete Backend Folder

--- Backend/src/routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { loginUser,} from "../controllers/user.controller.js";
-
-const router = Router();
-
-router.route("/login").post(loginUser);
-
-export default router

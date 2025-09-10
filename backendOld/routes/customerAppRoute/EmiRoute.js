1ddf8ad282f17849f38c57328f147826c68d4e68
kandalkarrohan <cropkhata@gmail.com>
2025-09-10 20:09:18 +0530

    Update

--- backendOld/routes/customerAppRoute/EmiRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { submitEmiForm } from "../../controllers/customerAppController/loanEmiController.js";
-
-
-const router = express.Router();
-
-router.post('/emiform',submitEmiForm)
-
-export default router;
--- backendOld/routes/salesAppRoute/enquiryRoute.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { assignEnquiry, getAll } from "../../controllers/salesApp/enquiryController.js";
-const router = express.Router();
-
-router.post("/assign/to/partner/:id", assignEnquiry);
-router.get('/getAll/:id',getAll)
-export default router;
